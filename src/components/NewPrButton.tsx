aaf37cfcb55c3f54bef478aea03bd4611f727f90
Suf <sufkarmon2@gmail.com>
2023-01-22 00:22:41 +0200

    add form to modal without labels

--- src/components/NewPrButton.tsx
+++ src/components/NewPrButton.tsx
@@ -6,9 +6,19 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import {FormControlLabel, FormLabel, Radio, RadioGroup, useTheme} from "@mui/material";
 
 export default function NewPrButton() {
     const [open, setOpen] = React.useState(false);
+    // const [value, setValue] = React.useState({});
+    //
+    // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     const newValue = event.target.value;
+    // }
+
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -21,28 +31,77 @@ export default function NewPrButton() {
     return (
         <div>
             <Button variant="outlined" onClick={handleClickOpen}>
-                Open form dialog
+                Create PR
             </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Subscribe</DialogTitle>
+            <Dialog open={open} onClose={handleClose} fullScreen={fullScreen}>
+                <DialogTitle>Create new pull request</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We
-                        will send updates occasionally.
+                        Please fill out the pull request details
                     </DialogContentText>
                     <TextField
+                        required
                         autoFocus
                         margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
+                        id="title"
+                        // value={value}
+                        label="Title"
+                        type="string"
                         fullWidth
                         variant="standard"
+                        // onChange={handleChange}
                     />
+                    <TextField
+                        margin="dense"
+                        id="description"
+                        // value={value}
+                        label="Description"
+                        type="string"
+                        fullWidth
+                        variant="standard"
+                        multiline
+                        maxRows={4}
+                        // onChange={handleChange}
+                    />
+                    <TextField
+                        required
+                        margin="dense"
+                        id="firstName"
+                        // value={value}
+                        label="First Name"
+                        type="string"
+                        fullWidth
+                        variant="standard"
+                        // onChange={handleChange}
+                    />
+                    <TextField
+                        required
+                        margin="dense"
+                        id="lastName"
+                        // value={value}
+                        label="Last name"
+                        type="string"
+                        fullWidth
+                        variant="standard"
+                        // onChange={handleChange}
+                    />
+                    <FormLabel id="demo-row-radio-buttons-group-label">Status</FormLabel>
+                    <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                        defaultValue= "open"
+                        // value={value}
+                        // onChange={handleChange}
+                    >
+                        <FormControlLabel value="open" control={<Radio />} label="Open"/>
+                        <FormControlLabel value="closed" control={<Radio />} label="Closed" />
+                        <FormControlLabel value="draft" control={<Radio />} label="Draft" />
+                    </RadioGroup>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Subscribe</Button>
+                    <Button onClick={handleClose}>Create PR</Button>
                 </DialogActions>
             </Dialog>
         </div>
