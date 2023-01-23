e066d0f9f8f54ea8f0e5b1180bf2c705ddd5bdf9
Suf <sufkarmon2@gmail.com>
2023-01-24 00:03:57 +0200

    add renderCellExpand, and fix table rows adjustment

--- src/components/PrTable.tsx
+++ src/components/PrTable.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {DataGrid, GridValueGetterParams} from '@mui/x-data-grid';
+import {DataGrid, GridColDef, GridValueGetterParams} from '@mui/x-data-grid';
 import {Box} from "@mui/material";
 import {useQuery} from "react-query";
 import axios from "axios";
 import moment from "moment-timezone"
+import renderCellExpand from "./renderCellExpand";
 
 type Author = {
     firstName: string;
@@ -32,27 +33,28 @@ export const PrTable: React.FC = () => {
     if (status === 'loading')
         return <h2>Loading...</h2>
 
-    const columns = [
-        {field: "title", headerName: "Title"},
-        {field: "description", headerName: "Description"},
+    const columns: GridColDef[] = [
+        {field: "title", headerName: "Title", width: 150, renderCell: renderCellExpand},
+        {field: "description", headerName: "Description", width: 200, renderCell: renderCellExpand},
         {
             field: "author",
             headerName: "Author",
             valueGetter: (params: GridValueGetterParams) =>
                 `${params.row.author.firstName || ''} ${params.row.author.lastName || ''}`,
+            renderCell: renderCellExpand
         },
-        {field: "createdAt", headerName: "Created Date", width: 300},
-        {field: "prNumber", headerName: "Number"},
-        {field: "status", headerName: "Status"},
-        {field: "labels", headerName: "Labels"},
-        {field: "id", headerName: "ID"},
+        {field: "createdAt", headerName: "Created Date", width: 140, renderCell: renderCellExpand},
+        {field: "prNumber", headerName: "Number", width: 70, type: 'number', renderCell: renderCellExpand},
+        {field: "status", headerName: "Status", type: 'singleSelect', valueOptions: ['Open', 'Closed', 'Draft'], renderCell: renderCellExpand},
+        {field: "labels", headerName: "Labels", renderCell: renderCellExpand},
+        {field: "id", headerName: "ID", renderCell: renderCellExpand},
     ];
 
-    const rows = data?.data.map((row: PullRequest) => ({
+    const rows: any = data?.data.map((row: PullRequest) => ({
         title: row.title,
         description: row.description,
         author: row.author,
-        createdAt: moment.tz(row.createdAt.toString(), "Israel"),
+        createdAt: moment.tz(row.createdAt.toString(), "Israel").format("YYYY-MM-DD HH:mm"),
         prNumber: row.prNumber,
         status: row.status,
         labels: row.labels,
